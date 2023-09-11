4fe982950ea7af333641af4b52ee5ebe375c2323
syoon6682 <dev.syoon6682@gmail.com>
2023-09-11 16:25:24 +0900

    ADD: predict.py & models.py variable

--- pricelog/models.py
+++ pricelog/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
 # Create your models here.
-class PriceLog(models.Model):
+class EstateLog(models.Model):
     product_id = models.IntegerField()
     price = models.IntegerField() # 거래금액
+    year = models.IntegerField() # 년
+    month = models.IntegerField() # 월
+    day = models.IntegerField() # 일
+    area = models.IntegerField() # 면적
     
-    ymd = models.CharField((""), max_length=50)
+    
+class LuxuryLog(models.Model):
+    luxury_id = models.IntegerField()
+    ymd = models.CharField(max_length=30) # 년월일
+    price = models.IntegerField() # 거래금액
+
+    
+class MusicLog(models.Model):
+    music_id = models.IntegerField()
+    ymd = models.CharField(max_length=30) # 년월일
+    price_high = models.IntegerField()
+    price_low = models.IntegerField()
+    price_close = models.IntegerField()
+    pct_price_change = models.IntegerField()
+    cnt_units_traded = models.IntegerField() 
+    
+
+    
+    
+    
