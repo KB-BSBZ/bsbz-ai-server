9b777a54c0b43c4cac1c043e1a3e2dc9cccfdaec
syoon6682 <dev.syoon6682@gmail.com>
2023-09-13 18:06:06 +0900

    ADD: migration file & csv total file

--- analysis/process.py
+++ analysis/process.py
@@ -1,17 +1,51 @@
 import pandas as pd
 import sqlite3
 
-# Read csv file.
-df = pd.read_csv("Book1.csv")
+
+# df = pd.read_csv(f"analysis\csv\estate_total.csv")
+
+# # Connect to (create) database.
+# database = "db.sqlite3"
+# conn = sqlite3.connect(database)
+# dtype={
+#     "product_id": "IntegerField",
+#     "price": "CharField",
+#     "year": "IntegerField", 
+#     "month": "IntegerField",
+#     "day": "IntegerField",
+#     "area": "FloatField",
+# }
+# df.to_sql(name='pricelog_estatelog', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
+# conn.close()
+
+#     print(f"estate{i} complete")
+
+# df = pd.read_csv(f"analysis\csv\music_total.csv")
+
+# # Connect to (create) database.
+# database = "db.sqlite3"
+# conn = sqlite3.connect(database)
+# dtype={
+#     "music_id": "IntegerField",
+#     "ymd": "CharField",
+#     "price_high": "IntegerField", 
+#     "price_low": "IntegerField",
+#     "price_close": "IntegerField",
+#     "pct_price_change": "FloatField",
+#     "cnt_units_traded": "IntegerField",
+# }
+# df.to_sql(name='pricelog_musiclog', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
+# conn.close()
+
+df = pd.read_csv(f"analysis\csv\luxury_total.csv")
 
 # Connect to (create) database.
 database = "db.sqlite3"
 conn = sqlite3.connect(database)
 dtype={
-    "title": "CharField",
-    "author": "CharField",
-    "rating": "IntegerField", 
-    "best seller": "BooleanField"
+    "luxury_id": "IntegerField",
+    "ymd" : "CharField",
+    "price": "IntegerField",
 }
-df.to_sql(name='home_book', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
+df.to_sql(name='pricelog_luxurylog', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
 conn.close()
